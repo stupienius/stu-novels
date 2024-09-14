9f5f890fbf747f8c8007a14617ce1b257b9c3504
stupienius <wangjay0401@gmail.com>
2024-09-14 15:51:33 +0800

    Homepage

---
+++ src/app/Homepage.js
@@ -0,0 +1,10 @@
+export default function Homepage(){
+  return (
+    <>
+      <input type="text"></input>
+      <button>search</button>
+      <p>recent chapter</p>
+      <h1>hello</h1>
+    </>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,42 @@
-import Image from "next/image";
+import Homepage from "./Homepage.js";
+
+const chapterList = [
+  {index:1,url:"https://www.google.com"},
+  {index:2,url:"https://youtube.com"}
+];
 
 export default function Home() {
   return (
-  <h1>hello world</h1>
+    <>
+      <Homepage />
+      <List />
+      <Chapter chapter_now="1" />
+      <h1>hello world</h1>
+    </>
+  );
+}
+
+function List(){
+  return (
+    <>
+      <ul>
+        {chapterList.map(chapter => (
+          <li>
+            <a href={chapter.url}>{chapter.index}</a>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+
+function Chapter({chapter_now}){
+  return (
+    <>
+      <p>
+        {chapter_now}
+      </p>
+    </>
   );
 }
